a231d95483ed71cba0fbc6358dfa2a02f4889a21
animesh-chakrabarty <animesh_chakrabarty@outlook.com>
2024-10-30 15:31:42 +0530

    join us section added

--- src/components/Home/JoinUs.tsx
+++ src/components/Home/JoinUs.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Button } from "../ui/button";
 
 const JoinUs = () => {
   return (
-    <div className='flex-[40%] border rounded-xl'></div>
-  )
-}
+    <div className="bg-gradient-to-l from-gray-400 to-gray-950 flex flex-col gap-4 text-white px-[60px] py-8 rounded-[28px] shadow-lg">
+      <p className="text-lg font-lato">Want to publish your own article ?</p>
+      <Button className=" hover:bg-white hover:text-black">Join Us Now</Button>
+      <p className="font-lato text-center">
+        Effortlessly write and publish blogs with Jotter, plus get the benefits
+        of hasslefree blog writing using our built-in text editor
+      </p>
+    </div>
+  );
+};
 
-export default JoinUs
+export default JoinUs;
